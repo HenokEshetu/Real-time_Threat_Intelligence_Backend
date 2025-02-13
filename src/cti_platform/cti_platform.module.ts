6f04edd86d99e4ee37e0703b54ca5e478817815b
balex <balex@localhost.localdomain>
2025-02-13 19:52:48 +0300

    file change

--- src/cti_platform/cti_platform.module.ts
+++ src/cti_platform/cti_platform.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AddStixReportManuallyModule } from './modules/add-stix-report-manually/add-stix-report-manually.module';
 import { AlertGenerationModule } from './modules/alert-generation/alert-generation.module';
 import { AnalysisAndThreatCorrelationModule } from './modules/analysis-and-threat-correlation/analysis-and-threat-correlation.module';
-import { DashboardAndVisualizationModule } from './modules/dashboard-and-visualization/dashboard-and-visualization.module';
 import { EnrichmentModule } from './modules/enrichment/enrichment.module';
 import { ExportStixReportModule } from './modules/export-stix-report/export-stix-report.module';
 import { IngestionFromApiFeedsModule } from './modules/ingestion-from-api-feeds/ingestion-from-api-feeds.module';
@@ -16,7 +15,6 @@ import { StixObjectsModule } from './modules/stix-objects/stix-objects.module';
     AddStixReportManuallyModule,
     AlertGenerationModule,
     AnalysisAndThreatCorrelationModule,
-    DashboardAndVisualizationModule,
     EnrichmentModule,
     ExportStixReportModule,
     IngestionFromApiFeedsModule,
