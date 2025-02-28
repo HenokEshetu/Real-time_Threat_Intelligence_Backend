91180a38de7829cf9c3d3f1acfd45e481e6898b8
balex29 <170674660+balex29@users.noreply.github.com>
2025-02-28 12:38:18 +0300

    Balemwal
    * file change
    
    * files change
    
    * types change
    
    * src/cti_platform/core/types/common-data-types.ts
    
    * Balemwal (#10)
    
    * file change
    
    * files change
    
    * types change
    
    ---------
    
    Co-authored-by: balex <balex@localhost.localdomain>
    
    * Balemwal (#11)
    
    * file change
    
    * files change
    
    * types change
    
    ---------
    
    Co-authored-by: balex <balex@localhost.localdomain>
    
    * i have implemented stix objects
    
    * correction at src/app.module.ts
    
    * Balemwal (#10)
    
    * file change
    
    * files change
    
    * types change
    
    ---------
    
    Co-authored-by: balex <balex@localhost.localdomain>
    
    * Balemwal (#11)
    
    * file change
    
    * files change
    
    * types change
    
    ---------
    
    Co-authored-by: balex <balex@localhost.localdomain>
    
    * Balemwal (#10)
    
    * file change
    
    * files change
    
    * types change
    
    ---------
    
    Co-authored-by: balex <balex@localhost.localdomain>
    
    * Balemwal (#11)
    
    * file change
    
    * files change
    
    * types change
    
    ---------
    
    Co-authored-by: balex <balex@localhost.localdomain>
    
    * Balemwal (#10)
    
    * file change
    
    * files change
    
    * types change
    
    ---------
    
    Co-authored-by: balex <balex@localhost.localdomain>
    
    * Balemwal (#11)
    
    * file change
    
    * files change
    
    * types change
    
    ---------
    
    Co-authored-by: balex <balex@localhost.localdomain>
    
    * i have implemented stix objects
    
    * correction at src/app.module.ts
    
    ---------
    
    Co-authored-by: balex <balex@localhost.localdomain>
    Co-authored-by: Henok Eshetu <53043206+HenokEshetu@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,41 +1,20 @@
-
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { UserManagementModule } from './user-management/user-management.module';
-import databaseConfig from './config/database.config';
-import authConfig from './config/auth.config';
-import { join } from 'path';
 import { CtiPlatformModule } from './cti_platform/cti_platform.module';
-
+import { UserManagementModule } from './user-management/user-management.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [databaseConfig, authConfig],
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        ...configService.get('database'),
-        autoLoadEntities: true,
-      }),
-    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: false, // Disable auto schema generation here
       sortSchema: true,
       playground: true,
       context: ({ req }) => ({ req }),
     }),
-    UserManagementModule,
     CtiPlatformModule,
-    
+    UserManagementModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
